22d56f236f67126f64c0048fbf7d65de97d727c2
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:41:33 +0600

    Create tempCodeRunnerFile.c

---
+++ ICPC 2023/Miloy vai SEP 21/Miloy vai SEP 26/tempCodeRunnerFile.c
@@ -0,0 +1,23 @@
+//prince
+
+#include<stdio.h>
+
+int main()
+{
+    long long int x,a,b;
+    scanf("%lld %lld %lld",&x,&a,&b);
+    if(b-a<=x)
+    {
+        printf("safe\n");
+    }
+    else if(b-a<=0)
+    {
+        printf("delicious\n");
+    }
+    else
+    {
+        printf("dangerous\n");
+    }
+
+    return 0;
+}
