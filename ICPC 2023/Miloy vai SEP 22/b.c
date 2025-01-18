969ea0cbcd7b2c43a35ace9ad993af911bead650
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:41:37 +0600

    Create b.c

---
+++ ICPC 2023/Miloy vai SEP 22/b.c
@@ -0,0 +1,17 @@
+//prince
+
+#include<stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        long long int n;
+        scanf("%lld",&n);
+        printf("%lld 1\n",n-1);
+    }
+    
+    return 0;
+}
